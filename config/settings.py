acf85c2ced1ceb37ac32b9abb01592c6da842d28
chidiarua <chidiarua99@gmail.com>
2024-07-01 14:27:41 +0100

    updated view logic and env config

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
@@ -137,6 +137,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE =  True
+SESSION_COOKIE_SECURE =  True
+SECURE_SSL_REDIRECT =  True
--- main/views.py
+++ main/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.views.generic import View
 import pyshorteners
 
-# Create your views here.
 
 
 class UrlHome(View):
     def post(self, request):
         long_url = "url" in request.POST and request.POST["url"]
         ps = pyshorteners.Shortener()
-        short_url = ps.chilpit.short(long_url)
+        short_url = ps.clckru.short(long_url)
         return render(
             request,
             "index.html",

17bed00d9264c9de30a976e1e1d1ea9e299a7d6a
chidiarua <chidiarua99@gmail.com>
2025-04-18 19:27:55 +0100

    more fixes

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Use Python 3
-python3 -m pip install -r requirements.txt
+# Upgrade pip first
+python3 -m pip install --upgrade pip
+
+# Install dependencies with verbose output
+python3 -m pip install -r requirements.txt --verbose
 
 # Set Python path
 export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+# Verify Django installation
+python3 -c "import django; print(django.__version__)"
 
 # Collect static files
 python3 manage.py collectstatic --noinput 

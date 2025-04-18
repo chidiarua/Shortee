78135de5d7c17cbeea9023514fa1d4c81dc7b04e
chidiarua <chidiarua99@gmail.com>
2025-04-18 19:22:23 +0100

    more fixes

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Use Python 3.10 explicitly
-python3.10 -m pip install -r requirements.txt
+# Use Python 3
+python3 -m pip install -r requirements.txt
 
 # Set Python path
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 # Collect static files
-python3.10 manage.py collectstatic --noinput 
+python3 manage.py collectstatic --noinput 
